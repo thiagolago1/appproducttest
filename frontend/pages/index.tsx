b4c7644be1e26ddd3edb5b2e54cfe4bcc6282371
Thiago Lago <thiagolagoo2@gmail.com>
2020-07-13 15:39:37 +0100

    :art: add material ui

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,12 +2,35 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchposts } from '../store/actions/postActions';
 import {useEffect} from 'react';
-import { MdPlusOne } from 'react-icons/md';
+import { MdAdd, MdEdit, MdDelete } from 'react-icons/md';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
 
 export default function Home() {
 
 const dispatch = useDispatch();
 const { posts } = useSelector(state => state.post);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  tableCell: {
+    fontWeight: "bold"
+  }
+});
+
+const classes = useStyles();
 
 useEffect(() => {
   dispatch(fetchposts());
@@ -15,35 +38,46 @@ useEffect(() => {
 
   return (
    <>
-   <div>
-      <h1 className="title">List of Products</h1>
 
-        <table className="container-table">
-          <tr>
-            <th>Name</th>
-            <th>price</th>
-          </tr>
+    <div className="table-container-m">
+    <Typography variant="h3" component="h2" gutterBottom>
+       Products List
+      </Typography>
 
-          {posts && posts.map(item => (
-              <tr key={item.id}>
-                <td>
-                  {item.name}
-                  </td>
-                  <td>
-                  € {item.price}
-                  </td>
-                </tr>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+              <TableCell className={classes.tableCell}></TableCell>
+                <TableCell className={classes.tableCell}><Typography variant="h6" gutterBottom>ID</Typography></TableCell>
+                <TableCell align="left" className={classes.tableCell}><Typography variant="h6" gutterBottom>Name</Typography></TableCell>
+                <TableCell align="left" className={classes.tableCell}><Typography variant="h6" gutterBottom>€ Price</Typography></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {posts.map((row) => (
+                <TableRow key={row._id}>
+                  <TableCell component="th" scope="row"><Typography variant="h6" component="h2" gutterBottom> <MdEdit/> <MdDelete /></Typography></TableCell>
+                  <TableCell component="th" scope="row"><Typography variant="h6" component="h2" gutterBottom>{row._id}</Typography></TableCell>
+                  <TableCell align="left"><Typography variant="h6" component="h2" gutterBottom>{row.name}</Typography></TableCell>
+                  <TableCell align="left"><Typography variant="h6" component="h2" gutterBottom>€ {row.price}</Typography></TableCell>
+                </TableRow>
               ))}
-        </table>
-
+            </TableBody>
+          </Table>
+        </TableContainer>
     </div>
 
     <div>
-      <h4 className="form-title">Add new product </h4>
       <form className="container-form">
-        <input placeholder="product name"/> <br />
-        <input placeholder="product price"/> <br />
-        <button>Add <MdPlusOne /></button>
+      <Typography variant="h5" component="h2" gutterBottom>
+       Add new product
+      </Typography>
+        <TextField id="standard-basic" label="name" /> <br />
+        <TextField id="standard-basic" label="price" /> <br /> <br />
+        <Button variant="contained" color="primary" disableElevation>
+            Add <MdAdd />
+        </Button>
       </form>
     </div>
    </>
--- frontend/pages/products.tsx
+++ frontend/pages/products.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const Product = ({ product }: { product: any}) => (
 
   <>
-    <div>
-      <h1>
-        List All Products
-      </h1>
-          <ul>
-            {product.map((products: { id: React.Key; name: React.ReactNode; price: React.ReactNode; }) => (
-              <li key={products.id}>
-                {products.name} - € {products.price}
-              </li>
-            ))}
-          </ul>
+    <div className="table-container-m">
+      <h1>List All Products</h1>
+
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {product.map((row) => (
+                <TableRow key={row._id}>
+                  <TableCell component="th" scope="row">
+                    {row._id}
+                  </TableCell>
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.price}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
     </div>
 
     <div>
